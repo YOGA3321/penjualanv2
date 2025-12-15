f63f59e9aaff1016538e202c0bf70df275c12780
YOGA3321 <ageng.prayoga321@gmail.com>
2025-12-15 21:02:47 +0700

    Fix: Remove double slash in Google Redirect URI

--- auth/google_config.php
+++ auth/google_config.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__) . '/koneksi.php';
 $clientID = $_ENV['GOOGLE_CLIENT_ID']; 
 $clientSecret = $_ENV['GOOGLE_CLIENT_SECRET'];
 // 3. Gunakan BASE_URL agar otomatis menyesuaikan (Localhost / Online)
-$redirectUri = BASE_URL . '/auth/google_callback.php'; 
+$redirectUri = BASE_URL . 'auth/google_callback.php'; 
 
 $client = new Google_Client();
 $client->setClientId($clientID);
